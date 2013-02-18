704e3e9224969ce12a475cd4933fad37cf14110f
Flukey <jstevenhall@gmail.com>
2013-02-18 18:36:06 +0100

    Adding metadate resource type class

---
+++ Model/Metadata/ResourceType.php
@@ -0,0 +1,65 @@
+<?php
+/*
+* This file is part of the ILLDataCiteDOIBundle package.
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @License  MIT License
+*/
+
+namespace ILL\DataCiteDOIBundle\Model\Metadata;
+
+/**
+ * The type of a resource. You may enter an additional free text description.
+ * Please see http://schema.datacite.org/meta/kernel-2.0/metadata.xsd for more detail.
+ */
+class ResourceType
+{
+	private $type;
+	private $resourceType;
+
+	/*
+ 	 * Please see http://schema.datacite.org/meta/kernel-2.0/include/datacite-resourceType-v1.0.xsd for valid
+ 	 * general types
+ 	 */
+	private static $RESOURCE_TYPES = array("Collection",
+								 "Dataset",
+								 "Event",
+								 "Film",
+								 "Image",
+								 "InteractiveResource",
+								 "PhysicalObject",
+								 "Service",
+								 "Software",
+								 "Sound",
+								 "Text");
+
+	public function setType($type)
+	{
+		if(null === $type) {
+			throw new \Exception("Type cannot be null");
+		}
+		$this->type = $type;
+		return $this;
+	}
+
+	public function getType() {
+		return $this->type;
+	}
+
+	public function setResourceType($resourceType)
+	{
+		if(in_array($resourceType, self::RESOURCE_TYPES)) {
+			$this->resourceType = $resourceType;
+			return $this;
+		} else {
+			throw new \Exception(sprintf("Not a valid resource type. Valid types are: %s", json_encode(self::RESOURCE_TYPES)));
+		}
+	}
+
+	public function getResourceType()
+	{
+		return $this->resourceType;
+	}
+}
