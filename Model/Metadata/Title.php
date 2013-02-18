6a789a832ae2f243d21599061d031eb42fdc2b6f
Flukey <jstevenhall@gmail.com>
2013-02-18 18:35:51 +0100

    Adding metadata title class

---
+++ Model/Metadata/Title.php
@@ -0,0 +1,52 @@
+<?php
+/*
+* This file is part of the ILLDataCiteDOIBundle package.
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @License  MIT License
+*/
+
+namespace ILL\DataCiteDOIBundle\Model\Metadata;
+
+/**
+ * A name or title by which a resource is known.
+ * Please see http://schema.datacite.org/meta/kernel-2.0/metadata.xsd for more detail.
+ */
+class Title
+{
+	private $title;
+	private $type;
+
+	/*
+ 	 * Please see http://schema.datacite.org/meta/kernel-2.0/include/datacite-titleType-v1.0.xsd for valid
+ 	 * title types
+ 	 */
+	private static $TYPES = array("AlternativeTitle", "Subtitle", "TranslatedTitle");
+
+	public function setTitle($title)
+	{
+		$this->title = $title;
+		return $this;
+	}
+
+	public function getTitle() {
+		return $this->title;
+	}
+
+	public function setType($type)
+	{
+		if(in_array($type, self::TYPES)) {
+			$this->type = $type;
+			return $this;
+		} else {
+			throw new \Exception(sprintf("Not a valid type. Valid types are: %s", json_encode(self::TYPES)));
+		}
+	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+}
