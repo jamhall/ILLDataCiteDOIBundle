7621414aa3ea419984f7228cbaf825e4c0f9c3d8
Flukey <jstevenhall@gmail.com>
2013-02-19 18:18:35 +0100

    modifying test configuration. Added a stub metadata test class

---
+++ Tests/Metadata/MetadataTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace ILL\DataCiteDOIBundle\Tests\Metadata;
+
+class MetadataTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        // assert that your calculator added the numbers correctly!
+        $this->assertTrue(true);
+    }
+}
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,5 +4,4 @@ $file = __DIR__.'/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
 }
-
 require_once $file;
