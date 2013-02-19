7819a1b9a49b00cbf62b96214eade875b5dcdc3b
Flukey <jstevenhall@gmail.com>
2013-02-19 14:49:28 +0100

    adding a name identifier class for the metadata

---
+++ Model/Metadata/NameIdentifier.php
@@ -0,0 +1,54 @@
+<?php
+/*
+* This file is part of the ILLDataCiteDOIBundle package.
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @License  MIT License
+*/
+
+namespace ILL\DataCiteDOIBundle\Model\Metadata\Validator;
+use ILL\DataCiteDOIBundle\Model\Metadata\Validator\NonEmptyStringValidator;
+
+/**
+ * The name identifier for a creator
+ * Please see http://schema.datacite.org/meta/kernel-2.0/metadata.xsd for more detail.
+ */
+class NameIdentifier
+{
+    private $scheme = null;
+    private $identifier;
+
+    public function setIdentifier($identifier)
+    {
+        if (null === $this->scheme) {
+            throw new \InvalidArgumentException("Please set the scheme before setting the identifier");
+        } else {
+            if (NonEmptyStringValidator::isValid("identifier", $identifier)) {
+                $this->identifier = $identifier;
+            }
+        }
+
+        return $this;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    public function setScheme($scheme)
+    {
+        if (NonEmptyStringValidator::isValid("scheme", $scheme)) {
+            $this->scheme = $scheme;
+        }
+
+        return $this;
+    }
+
+    public function getNameIdentifier()
+    {
+        return $this->nameIdentifier;
+    }
+}
