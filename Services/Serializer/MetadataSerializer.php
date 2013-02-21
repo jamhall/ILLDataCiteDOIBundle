0c8d88024b61d978dd9bb9bcc7df2507a13fe907
Flukey <jstevenhall@gmail.com>
2013-02-21 18:27:14 +0100

    complete serialization of metadata model into XML

--- Services/Serializer/MetadataSerializer.php
+++ Services/Serializer/MetadataSerializer.php
@@ -41,8 +41,203 @@ class MetadataSerializer
         $root->setAttributeNS(self::W3_XML_SCHEMA_NAMESPACE, 'schemaLocation', sprintf("%s %s",
                                                                                        self::DATACITE_KERNEL_RESOURCE_NAMESPACE,
                                                                                        self::DATACITE_METADATA_XML_SCHEMA));
+        /**
+         * Add identifier
+         */
+        $identifierElement = $xml->createElement("identifier", $metadata->getIdentifier());
+        $identifierElement->setAttribute("identifierType", "DOI");
+        $root->appendChild($identifierElement);
 
+        /**
+         * Add creators
+         */
+        $creatorsElement = $xml->createElement('creators');
+        $root->appendChild($creatorsElement);
+        foreach ($metadata->getCreators() as $creator) {
+            $creatorElement = $xml->createElement("creator");
+            $creatorNameElement = $xml->createElement("creatorName", $creator->getName());
+            $creatorsElement->appendChild($creatorElement);
+            $creatorElement->appendChild($creatorNameElement);
+            if ($creator->getNameIdentifiers()) {
+                foreach ($creator->getNameIdentifiers() as $nameIdentifier) {
+                    $nameIdentifierElement = $xml->createElement("nameIdentifier", $nameIdentifier->getIdentifier());
+                    $nameIdentifierElement->setAttribute("nameIdentifierScheme", $nameIdentifier->getScheme());
+                    $creatorElement->appendChild($nameIdentifierElement);
+                }
+            }
+        }
 
+        /**
+         * Add titles
+         */
+        $titlesElement = $xml->createElement('titles');
+        $root->appendChild($titlesElement);
+        foreach ($metadata->getTitles() as $title) {
+            $titleElement = $xml->createElement("title", $title->getTitle());
+            if ($title->getType()) {
+                $titleElement->setAttribute("titleType", $title->getType());
+            }
+            $titlesElement->appendChild($titleElement);
+        }
+
+        /**
+         * Add publisher and publication year
+         */
+        $root->appendChild($xml->createElement("publisher", $metadata->getPublisher()));
+        $root->appendChild($xml->createElement("publicationYear", $metadata->getPublicationYear()));
+
+        /**
+         * Add subjects (if any)
+         */
+        if ($metadata->getSubjects()) {
+            $subjectsElement = $xml->createElement('subjects');
+            $root->appendChild($subjectsElement);
+            foreach ($metadata->getSubjects() as $subject) {
+                $subjectElement = $xml->createElement("subject", $subject->getSubject());
+                if ($subject->getScheme()) {
+                    $subjectElement->setAttribute("subjectScheme", $subject->getScheme());
+                }
+                $subjectsElement->appendChild($subjectElement);
+            }
+        }
+
+        /**
+         * Add contributors (if any)
+         */
+        if ($metadata->getContributors()) {
+            $contributorsElement = $xml->createElement('contributors');
+            $root->appendChild($contributorsElement);
+            foreach ($metadata->getContributors() as $contributor) {
+                $contributorElement = $xml->createElement("contributor");
+                $contributorElement->setAttribute("contributorType", $contributor->getType());
+                $contributorNameElement = $xml->createElement("contributorName", $creator->getName());
+                $contributorsElement->appendChild($contributorElement);
+                $contributorElement->appendChild($contributorNameElement);
+                if ($contributor->getNameIdentifiers()) {
+                    foreach ($contributor->getNameIdentifiers() as $nameIdentifier) {
+                        $nameIdentifierElement = $xml->createElement("nameIdentifier", $nameIdentifier->getIdentifier());
+                        $nameIdentifierElement->setAttribute("nameIdentifierScheme", $nameIdentifier->getScheme());
+                        $contributorElement->appendChild($nameIdentifierElement);
+                    }
+                }
+            }
+        }
+
+        /**
+         * Add dates (if any)
+         */
+        if ($metadata->getDates()) {
+            $datesElement = $xml->createElement('dates');
+            $root->appendChild($datesElement);
+            foreach ($metadata->getDates() as $date) {
+                $dateElement = $xml->createElement("date", $date->getDate());
+                $dateElement->setAttribute("dateType", $date->getType());
+                $datesElement->appendChild($dateElement);
+            }
+        }
+
+        /**
+         * Add resource type (if specified)
+         */
+        if ($metadata->getResourceType()) {
+            $resourceType = $xml->createElement("resourceType", $metadata->getResourceType()->getType());
+            $resourceType->setAttribute("resourceTypeGeneral", $metadata->getResourceType()->getResourceType());
+            $root->appendChild($resourceType);
+        }
+
+        /**
+         * Add alternate identifiers (if any)
+         */
+        if ($metadata->getAlternateIdentifiers()) {
+            $alternateIdentifiersElement = $xml->createElement('alternateIdentifiers');
+            $root->appendChild($alternateIdentifiersElement);
+            foreach ($metadata->getAlternateIdentifiers() as $alternateIdentifier) {
+                $alternateIdentifierElement = $xml->createElement("alternateIdentifier", $alternateIdentifier->getIdentifier());
+                $alternateIdentifierElement->setAttribute("alternateIdentifierType", $alternateIdentifier->getType());
+                $alternateIdentifiersElement->appendChild($alternateIdentifierElement);
+            }
+        }
+
+        /**
+         * Add related identifiers (if any)
+         */
+        if ($metadata->getRelatedIdentifiers()) {
+            $relatedIdentifiersElement = $xml->createElement('relatedIdentifiers');
+            $root->appendChild($relatedIdentifiersElement);
+            foreach ($metadata->getRelatedIdentifiers() as $relatedIdentifier) {
+                $relatedIdentifierElement = $xml->createElement("relatedIdentifier", $relatedIdentifier->getIdentifier());
+                $relatedIdentifierElement->setAttribute("relatedIdentifierType", $relatedIdentifier->getRelatedIdentifierType());
+                $relatedIdentifierElement->setAttribute("relationType", $relatedIdentifier->getRelationType());
+                $relatedIdentifiersElement->appendChild($relatedIdentifierElement);
+            }
+        }
+
+        /**
+         * Add sizes (if any)
+         */
+        if ($metadata->getSizes()) {
+            $sizesElement = $xml->createElement('sizes');
+            $root->appendChild($sizesElement);
+            foreach ($metadata->getSizes() as $size) {
+                $sizesElement->appendChild($xml->createElement("size", $size->getSize()));
+            }
+        }
+
+        /**
+         * Add formats (if any)
+         */
+        if ($metadata->getFormats()) {
+            $formatsElement = $xml->createElement('formats');
+            $root->appendChild($formatsElement);
+            foreach ($metadata->getFormats() as $format) {
+                $formatsElement->appendChild($xml->createElement("format", $format->getFormat()));
+            }
+        }
+
+        /**
+         * Set version (if specified)
+         */
+        if ($metadata->getVersion()) {
+            $root->appendChild($xml->createElement("version", $metadata->getVersion()));
+        }
+
+        /**
+         * Set rights (if specified)
+         */
+        if ($metadata->getRights()) {
+            $root->appendChild($xml->createElement("rights", $metadata->getRights()));
+        }
+
+        /**
+         * Add descriptions (if any)
+         */
+        if ($metadata->getDescriptions()) {
+            $descriptionsElement = $xml->createElement('descriptions');
+            $root->appendChild($descriptionsElement);
+            foreach ($metadata->getDescriptions() as $description) {
+                $descriptionElement = $xml->createElement("description", $description->getDescription());
+                if ($description->getType()) {
+                    $descriptionElement->setAttribute("descriptionType", $description->getType());
+                }
+                $descriptionsElement->appendChild($descriptionElement);
+            }
+        }
+
+        /**
+         * Validate the XML produced against the Metadata XSD
+         * @var [type]
+         */
+        $validate = new \DOMDocument;
+        $validate->loadXML($xml->savexml());
+
+        try {
+            if ($validate->schemaValidate("/home/hall/workspace/illdatacitedoibundle/Model/Metadata/Schema/metadata.xsd")) {
+                return $xml->savexml();
+            }
+        } catch (\ErrorException $e) {
+            throw new \Exception("The XML could not be validated: " . $e->getMessage());
+        }
+        #file_put_contents("/tmp/test.xml", $xml->savexml());
     }
 
     /**
