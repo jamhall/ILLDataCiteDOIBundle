1d37db7c5863d7059460883c3e06b33e2a23cb42
Flukey <jstevenhall@gmail.com>
2013-02-21 12:32:55 +0100

    first draft of serializer

--- Services/Serializer/MetadataSerializer.php
+++ Services/Serializer/MetadataSerializer.php
@@ -19,11 +19,29 @@ use ILL\DataCiteDOIBundle\Model\Metadata;
 class MetadataSerializer
 {
     /**
+     * XML namespaces and XML schemas
+     */
+    const W3_XML_NAMESPACE = "http://www.w3.org/2000/xmlns/";
+    const DATACITE_KERNEL_RESOURCE_NAMESPACE = "http://datacite.org/schema/kernel-2.1";
+    const W3_XML_SCHEMA_NAMESPACE = "http://www.w3.org/2001/XMLSchema-instance";
+    const DATACITE_METADATA_XML_SCHEMA = "http://schema.datacite.org/meta/kernel-2.1/metadata.xsd";
+
+    /**
      * Serialize metadata model into XML for the API
      * @return string
      */
-    public function serialize(Metadata $metadata)
+    public static function serialize(Metadata $metadata)
     {
+        // create a new XML document
+        $xml = new \DomDocument('1.0', 'UTF-8');
+        // create root node
+        $root = $xml->createElementNS(self::DATACITE_KERNEL_RESOURCE_NAMESPACE, 'resource');
+        $root = $xml->appendChild($root);
+        $root->setAttributeNS(self::W3_XML_NAMESPACE ,'xmlns:xsi', self::W3_XML_SCHEMA_NAMESPACE);
+        $root->setAttributeNS(self::W3_XML_SCHEMA_NAMESPACE, 'schemaLocation', sprintf("%s %s",
+                                                                                       self::DATACITE_KERNEL_RESOURCE_NAMESPACE,
+                                                                                       self::DATACITE_METADATA_XML_SCHEMA));
+
 
     }
 
@@ -31,7 +49,7 @@ class MetadataSerializer
      * Unserialize XML returned from the API into a metadata model
      * @return object Metadata
      */
-    public function unserialize($xml)
+    public static function unserialize($xml)
     {
 
     }
