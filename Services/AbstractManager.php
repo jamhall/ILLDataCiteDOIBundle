db76f2bb908618de9ce3ee091e2c8b9540d39a0e
Flukey <jstevenhall@gmail.com>
2013-02-27 16:22:26 +0100

    adding functionality to load response codes config file

--- Services/AbstractManager.php
+++ Services/AbstractManager.php
@@ -10,6 +10,8 @@
 
 namespace ILL\DataCiteDOIBundle\Services;
 use ILL\DataCiteDOIBundle\Http\Adapter\CurlAdapter;
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\Exception\ParseException;
 
 /**
  * @author Jamie Hall <hall@ill.eu>
@@ -18,6 +20,11 @@ abstract class AbstractManager
 {
     protected $adapter;
     protected $logger;
+    /**
+     * YAML object of expected API methods response codes
+     * @var [type]
+     */
+    protected static $responseCodes;
     /**
      * @var array $defaults Array of default options
      */
@@ -36,5 +43,25 @@ abstract class AbstractManager
         $this->defaults = array_merge($this->defaults, $options);
         $this->adapter = new CurlAdapter($this->defaults);
         $this->logger = $logger;
+        self::$responseCodes = $this->loadResponseCodes();
+    }
+
+    private function loadResponseCodes()
+    {
+        $yaml = new Parser();
+        try {
+            return $yaml->parse(file_get_contents(__DIR__.'/../Resources/config/api_response_codes.yml'));
+        } catch (ParseException $e) {
+            throw new ParseException(sprintf("Unable to parse the api_response_codes.yml file: %s", $e->getMessage()));
+        }
+    }
+
+    public function getResponseCodes($method)
+    {
+        try {
+            return self::$responseCodes[$method]['codes'];
+        } catch (\Exception $e) {
+            throw new \Exception(sprintf("The method %s was not found in the codes yaml file", $method));
+        }
     }
 }
