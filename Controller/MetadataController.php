19c86c7cf668d774f33172b25601c2b22d604e68
Flukey <jstevenhall@gmail.com>
2013-03-21 16:53:54 +0100

    updated main layout. Added licensing text to controllers

--- Controller/MetadataController.php
+++ Controller/MetadataController.php
@@ -1,4 +1,12 @@
 <?php
+/*
+* This file is part of the ILLDataCiteDOIBundle package.
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @License  MIT License
+*/
 
 namespace ILL\DataCiteDOIBundle\Controller;
 
