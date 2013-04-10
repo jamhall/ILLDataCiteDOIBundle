e67d4b9573d78cda0b7f9bb47e8bb1cf147a04e2
Flukey <jstevenhall@gmail.com>
2013-04-10 13:53:18 +0200

    running php-cs-fixer

--- Validator/Constraints/MetadataIdentifierValidator.php
+++ Validator/Constraints/MetadataIdentifierValidator.php
@@ -7,14 +7,14 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class MetadataIdentifierValidator extends ConstraintValidator
 {
-	public function __construct($prefix = null)
-	{
-		$this->prefix = $prefix;
-	}
+    public function __construct($prefix = null)
+    {
+        $this->prefix = $prefix;
+    }
 
     public function validate($value, Constraint $constraint)
     {
-    	if($this->prefix == null) return true;
+        if($this->prefix == null) return true;
 
         if (!preg_match(sprintf('/^(%s\/[-_a-zA-Z0-9.:\+]+)/', addcslashes($this->prefix, ".")), $value, $matches)) {
             $this->context->addViolation($constraint->message, array('%string%' => $value));
